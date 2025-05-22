8025ec92980a13d90fb2d687ed7409292c2af1ea
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-22 10:10:38 +0700

    menambahkan void pop

--- file1.cpp
+++ file1.cpp
@@ -32,4 +32,15 @@ int push(int value){
     cout << "Push value: " << value << endl;
     return value;
 }
+
+// Pop operation : Remove the topmost element from the stack
+void pop(){
+    if (isEmpty()) {
+        cout << "Stack is empty." << endl;
+    }
+
+    Node* temp = top; // Create a temporary poiter to the top node
+    top = top->next; // Update the top pointer to the next node
+    cout <<"Popped value: " <<top->data << endl;
+}
 }

a2f509a85196d214b597fef15493ef0abdb62307
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-22 10:30:08 +0700

    menambahkan case 1 di switch int main

--- file1.cpp
+++ file1.cpp
@@ -75,7 +75,7 @@ bool isEmpty()
 
 int main()
 {
-    stack stack;
+    Stack stack;
 
      int choice = 0;
      int value;
@@ -88,5 +88,14 @@ int main()
         cout << "4. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value); //Push the entered onto the stack
+            break;
+        }
      }
 }

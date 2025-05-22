020c09d91f4db6ef0e6662af7deef94bd8fff444
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-22 10:37:44 +0700

    menambahkan cout di case 3 switch int main

--- file1.cpp
+++ file1.cpp
@@ -112,6 +112,10 @@ int main()
                 stack.peek(); //Get the value of the top element
             }
             else
+            {
+                cout << "Stack is empty. No top value." << endl;
+            }
+            break;
         }
      }
 }

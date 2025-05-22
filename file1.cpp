f20b1f6689cce5c990fd19d3777d1d9ba03f3bef
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-22 10:36:50 +0700

    menambahkan if else di case 3 switch int main

--- file1.cpp
+++ file1.cpp
@@ -105,6 +105,13 @@ int main()
             {
                 cout << "Stack is empty. Cannot pop." << endl;
             }
+            break;
+        case 3:
+            if (!stack.isEmpty())
+            {
+                stack.peek(); //Get the value of the top element
+            }
+            else
         }
      }
 }

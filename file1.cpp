469484d6fc31a7fdf28199d9cdfe87ae22a2dfb3
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-22 10:02:02 +0700

    menambahkan int puush(int value)  di class stack

--- file1.cpp
+++ file1.cpp
@@ -22,4 +22,14 @@ public:
     Stack(){
         top = NULL; //Initialize the stack with a null top pointer
     }
+
+// Push operation Insert an element onto the top of the stack
+int push(int value){
+    Node* newNode = new Node(); // 1. Allocate memory for the new node
+    newNode->data = value; // 2. Assign Value 
+    newNode->next = top; // 3. Set the next pointer of the new node to the current top
+    top = newNode; // 4. Update the top pointer to the new node
+    cout << "Push value: " << value << endl;
+    return value;
+}
 }
